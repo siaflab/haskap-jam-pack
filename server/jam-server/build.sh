ac5d79c8acaea6aca5a2c5752a17a2dda80834d8
kn1kn1 <kn1kn1@users.noreply.github.com>
2016-02-25 16:12:33 +0900

    Build 0.1.0-alpha1 binary

--- server/jam-server/build.sh
+++ server/jam-server/build.sh
@@ -12,7 +12,6 @@ SRC_DIR=.
 OUT_DIR=bin
 
 mkdir -p $OUT_DIR
-export GO15VENDOREXPERIMENT=1
 
 export GOOS=darwin;export GOARCH=amd64;go build -ldflags "-X main.BuildVersion=$BUILD_VERSION -X main.GitCommit=$GIT_COMMIT -X main.BuildDate=$BUILD_DATE" -o ${OUT_DIR}/${GOOS}_${GOARCH}/haskap-jam-server ${SRC_DIR}/haskap-jam-server.go
 export GOOS=windows;export GOARCH=amd64;go build -ldflags "-X main.BuildVersion=$BUILD_VERSION -X main.GitCommit=$GIT_COMMIT -X main.BuildDate=$BUILD_DATE" -o ${OUT_DIR}/${GOOS}_${GOARCH}/haskap-jam-server ${SRC_DIR}/haskap-jam-server.go
