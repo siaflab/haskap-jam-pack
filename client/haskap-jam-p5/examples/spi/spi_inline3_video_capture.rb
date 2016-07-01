8781eb4dcde5388f6a24daae8bbe1462daf31828
kn1kn1 <kn1kn1@users.noreply.github.com>
2016-07-01 18:51:35 +0900

    Add video capture example

---
+++ client/haskap-jam-p5/examples/spi/spi_inline3_video_capture.rb
@@ -0,0 +1,21 @@
+# Welcome to Sonic Pi v2.10
+
+load "~/github/haskap-jam-pack/client/haskap-jam-p5/haskap-jam-p5.rb"
+# rp5_inline_sketch
+rp5_inline_sketch({:full_screen => true})
+
+load_library :video
+include_package "processing.video"
+
+def setup
+  smooth
+  #  size(640, 480)
+  size displayWidth, displayHeight
+  @video = Capture.new(self, width, height, 30)
+  @video.start
+end
+
+def draw
+  @video.read if @video.available?
+  image(@video, 0, 0)
+end
